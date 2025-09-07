b5fccfae873b6585abf1cc82e7650011ab98c0a8
PedroAugusto2101 <pedrotiagobh@gmail.com>
2025-09-06 22:23:59 -0300

    sql command and annotations

--- 01_select.sql
+++ 01_select.sql
@@ -1,1 +1,8 @@
+-- select statement is used to fetch data from a database or to display a message
+-- the result is stored in a result table, called the result-set
+-- the basic syntax of select statement is:
+-- SELECT column1, column2, ...
+-- or SELECT 'string'
+-- or SELECT expression
 SELECT 'Hello World'
+-- we use the shotcut ctrl + shift + q to choose the database
