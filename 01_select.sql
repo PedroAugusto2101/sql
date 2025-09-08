e6a18ceae1360a8cf91a2c6dcb9170971c7cbd55
PedroAugusto2101 <pedrotiagobh@gmail.com>
2025-09-08 19:13:25 -0300

    finalize select clause

--- 01_select.sql
+++ 01_select.sql
@@ -4,5 +4,19 @@
 -- SELECT column1, column2, ...
 -- or SELECT 'string'
 -- or SELECT expression
-SELECT 'Hello World'
--- we use the shotcut ctrl + shift + q to choose the database
+SELECT 'Hello World';
+-- we use the shortcut ctrl + shift + q to choose the database
+-- and then we use again to run the queries
+SELECT 1 + 2;
+-- we can make calculations in the select statement
+-- SELECT doesn't need a table to work
+SELECT * FROM clientes;
+-- the * means all columns
+SELECT IdCliente,
+       QtdePontos,
+       DtCriacao
+
+FROM clientes
+LIMIT 10;
+-- we can select specific columns
+-- the LIMIT clause is used to specify the number of records to return
