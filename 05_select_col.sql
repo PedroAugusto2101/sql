7037713c9d028ffea19a6abdb724ef58dc985d21
PedroAugusto2101 <pedrotiagobh@gmail.com>
2025-09-13 21:24:56 -0300

    finalize new columns section

--- 05_select_col.sql
+++ 05_select_col.sql
@@ -1,11 +1,15 @@
 SELECT IdCliente,
+        QtdePontos,
         QtdePontos + 10 AS QtdePontosPlus10,
-        QtdePontos * 2 AS QtdePontosDouble
+        QtdePontos * 2 AS QtdePontosDouble,
 -- this creation of new column doesn't change the table structure
 -- in the database
 -- it just creates a new column in the result set
 -- because it's just a query, and we can rename it with AS (alias)
-        DtCriacao
+        DtCriacao,
+        substr(DtCriacao, 1, 19) AS dtSubString,
+        datetime(substr(DtCriacao, 1, 19)) AS dtCriacaoNova,
+        strftime('%w', datetime(substr(DtCriacao, 1, 19))) AS diaSemana
 -- we have sql functions, that depends on the sql flavor
 -- these functions can be used in the select statement
 -- for example, to get only the year from a date column
