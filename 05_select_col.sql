af5257536d1db3451bacf8ad91055bb8c8e217db
PedroAugusto2101 <pedrotiagobh@gmail.com>
2025-09-13 21:03:16 -0300

    att readme

---
+++ 05_select_col.sql
@@ -0,0 +1,14 @@
+SELECT IdCliente,
+        QtdePontos + 10 AS QtdePontosPlus10,
+        QtdePontos * 2 AS QtdePontosDouble
+-- this creation of new column doesn't change the table structure
+-- in the database
+-- it just creates a new column in the result set
+-- because it's just a query, and we can rename it with AS (alias)
+        DtCriacao
+-- we have sql functions, that depends on the sql flavor
+-- these functions can be used in the select statement
+-- for example, to get only the year from a date column
+-- we can use the YEAR() function in MySQL
+-- in other databases, the function might be different
+FROM clientes;
